85435de62a481bd3f83155efe902a2eaf4f6d195
Danilo Silva <danilo.silva@msn.com>
2023-07-23 20:07:42 -0300

    feat: Aula sobre Console.log

---
+++ aula007/index.js
@@ -0,0 +1,8 @@
+console.log("Danilo Silva"); // String = Texto (Utizando aspas duplas)
+console.log('Danilo "Silva"'); // String = Texto (Utizando aspas simples)
+console.log(`'Danilo' "Silva"`); // String = Texto (Utizando Template String)
+
+console.log(123456); // Number = Número Inteiro
+console.log(15.85); // Number = Número Decimal
+
+console.log(35, 15.85, "Danilo Silva"); // Podemos imprimir mais de um valor na mesma linha
