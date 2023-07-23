b51998d0dbdc9c7d5b210424eb3aa19bae372736
Danilo Silva <danilo.silva@msn.com>
2023-07-23 20:14:39 -0300

    feat: Exercício sobre Console.log

---
+++ Secao02/aula008/exer01.js
@@ -0,0 +1,11 @@
+// Exercício 01
+// Imprima no console o seguinte texto: Meu nome é "NOME". Estou aprendendo JavaScript às 10 da manhã.
+
+// Versão 01
+console.log(`Meu nome é "NOME". Estou aprendendo JavaScript às ${10} da manhã.`)
+
+// Versão 02
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às ' + 10 + ' da manhã.')
+
+// Versão 03
+console.log('Meu nome é "NOME". Estou aprendendo JavaScript às', 10, 'da manhã.')
