17a00cbefede191f82090fc6f8bb672e02418964
Danilo Silva <danilo.silva@msn.com>
2023-07-24 10:54:22 -0300

    feat: Aula sobre let vs var - Primeira Diferença

---
+++ Secao02/aula13/index.js
@@ -0,0 +1,13 @@
+// ECMAScript 2015 (ES6)
+var nome = 'Silva';
+// O var permite que a variável seja redeclarada
+var nome = 'Danilo';
+
+console.log(nome);
+
+// É possível declarar uma variável sem atribuir o tipo, mas não é recomendado
+teste = 'teste'; // Não faça isso
+
+// Se eu utilizar let, não posso redeclarar a variável
+let nome2 = 'Danilo';
+// let nome2 = 'Silva'; // Erro
